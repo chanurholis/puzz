b7a67bee9d0c1abd2989a6fe8020812bad256923
Chacha Nurholis <chachanurholis29@gmail.com>
2023-05-07 20:50:12 +0700

    improve: blog page - remove unused

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,25 +1,14 @@
-import CssBaseline from '@mui/material/CssBaseline';
 import Grid from '@mui/material/Grid';
 import Container from '@mui/material/Container';
-import GitHubIcon from '@mui/icons-material/GitHub';
-import FacebookIcon from '@mui/icons-material/Facebook';
-import TwitterIcon from '@mui/icons-material/Twitter';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 import Header from '@components/Header';
 import MainFeaturedPost from '@components/MainFeaturedPost';
 import FeaturedPost from '@components/FeaturedPost';
-import Main from '@components/Main';
-import Sidebar from '@components/Sidebar';
+import ListArticle from '@components/ListArticle';
 import Footer from '@components/Footer';
 
-import articles from '../src/articles.json';
-
-const sections = [
-  { title: 'Most Emailed', url: '#' },
-  { title: 'Most Shared', url: '#' },
-  { title: 'Most Viewed', url: '#' },
-];
+import articles from '@constants/articles.json';
+import sections from '@constants/sections.json';
 
 const mainFeaturedPost = {
   title: 'Title of a longer featured blog post',
@@ -49,47 +38,20 @@ const featuredArticles = [
   },
 ];
 
-const sidebar = {
-  title: 'About',
-  description:
-    'Etiam porta sem malesuada magna mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.',
-  archives: [
-    { title: 'March 2020', url: '#' },
-    { title: 'February 2020', url: '#' },
-    { title: 'January 2020', url: '#' }
-  ],
-  social: [
-    { name: 'GitHub', icon: GitHubIcon },
-    { name: 'Twitter', icon: TwitterIcon },
-    { name: 'Facebook', icon: FacebookIcon },
-  ],
-};
-
-const theme = createTheme();
-
 const Blog = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
+    <>
       <Container maxWidth="lg">
         <Header title="Puzz News" sections={sections} />
         <main>
           <MainFeaturedPost post={mainFeaturedPost} />
           <Grid container spacing={4}>
             {featuredArticles.map((post) => (
-              <FeaturedPost key={post.title} post={post} />
+              <FeaturedPost key={post?.title} post={post} />
             ))}
           </Grid>
           <Grid container spacing={5} sx={{ mt: 3 }}>
-            
-            <Main title="Articles" articles={articles} />
-
-            <Sidebar
-              title={sidebar.title}
-              description={sidebar.description}
-              archives={sidebar.archives}
-              social={sidebar.social}
-            />
+            <ListArticle header="Articles" articles={articles} />
           </Grid>
         </main>
       </Container>
@@ -97,7 +59,7 @@ const Blog = () => {
         title="Footer"
         description="Something here to give the footer a purpose!"
       />
-    </ThemeProvider>
+    </>
   );
 }
 
