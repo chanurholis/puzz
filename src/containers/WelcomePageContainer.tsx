f871b19d8cb403f286998e5b94567c2294e4ca2a
Chacha Nurholis <chachanurholis29@gmail.com>
2023-05-07 16:18:04 +0700

    feat: add welcome page container

---
+++ src/containers/WelcomePageContainer.tsx
@@ -0,0 +1,13 @@
+import { Container } from '@mui/material';
+
+import ButtomNavigationComponent from '@components/ButtomNavigationComponent';
+
+const WelcomePage = () => {
+  return (
+    <Container>
+      <ButtomNavigationComponent />
+    </Container>
+  );
+}
+
+export default WelcomePage;
