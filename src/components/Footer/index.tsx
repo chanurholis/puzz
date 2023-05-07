d00dc6c52264de18b38b6f68e155d1e150c93f37
Chacha Nurholis <chachanurholis29@gmail.com>
2023-05-08 06:09:55 +0700

    improve: footer compoonent

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,27 +1,15 @@
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
-import Link from '@mui/material/Link';
 
-function Copyright() {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
+import Copyright from '@components/Copyright';
 
 interface FooterProps {
   description: string;
   title: string;
 }
 
-export default function Footer(props: FooterProps) {
+const Footer = (props: FooterProps) => {
   const { description, title } = props;
 
   return (
@@ -43,3 +31,5 @@ export default function Footer(props: FooterProps) {
     </Box>
   );
 }
+
+export default Footer;
