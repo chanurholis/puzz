20bc04615a878bc61d62711e66de8e29d56a3e12
Chacha Nurholis <chachanurholis29@gmail.com>
2023-05-08 06:10:10 +0700

    improve: header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,41 +1,34 @@
 import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import SearchIcon from '@mui/icons-material/Search';
 import Typography from '@mui/material/Typography';
 import Link from '@mui/material/Link';
+import IconButton from '@mui/material/IconButton';
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
 
 interface HeaderProps {
-  sections: ReadonlyArray<{
-    title: string;
-    url: string;
-  }>;
+  sections: ReadonlyArray<{ title: string; url: string; }>;
   title: string;
 }
 
-export default function Header(props: HeaderProps) {
+const Header = (props: HeaderProps) => {
   const { sections, title } = props;
 
   return (
     <>
       <Toolbar sx={{ borderBottom: 1, borderColor: 'divider' }}>
-        <Button size="small">Subscribe</Button>
         <Typography
           component="h2"
           variant="h5"
           color="inherit"
-          align="center"
+          align="left"
           noWrap
           sx={{ flex: 1 }}
         >
           {title}
         </Typography>
         <IconButton>
-          <SearchIcon />
+          <AccountCircleOutlinedIcon fontSize="large" />
         </IconButton>
-        <Button variant="outlined" size="small">
-          Sign up
-        </Button>
       </Toolbar>
       <Toolbar
         component="nav"
@@ -58,3 +51,5 @@ export default function Header(props: HeaderProps) {
     </>
   );
 }
+
+export default Header;
