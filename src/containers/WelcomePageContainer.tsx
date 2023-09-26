f64237338c924d8860c1a5c34602929c727ac57d
Chacha Nurholis <chachanurholis29@gmail.com>
2023-09-26 20:57:31 +0700

    improve: welcome page container

--- src/containers/WelcomePageContainer.tsx
+++ src/containers/WelcomePageContainer.tsx
@@ -1,23 +1,18 @@
-import { Grid } from '@mui/material';
-
 import DefaultLayout from '@layouts/DefaultLayout';
 
 import ListArticle from '@components/ListArticle';
 import MainFeaturedArticle from '@components/MainFeaturedArticle';
 import ListFeaturedArticle from '@components/ListFeaturedArticle';
 
-import articles from '@constants/articles.json';
-import { featuredArticles, mainFeaturedArticle } from './dummy';
+import { articles, featuredArticles, mainFeaturedArticleData } from '@constants/dummy-data';
 
 const WelcomePage = () => {
   return (
     <>
       <DefaultLayout>
-        <MainFeaturedArticle post={mainFeaturedArticle} />
+        <MainFeaturedArticle article={mainFeaturedArticleData} />
         <ListFeaturedArticle featuredArticles={featuredArticles} />
-        <Grid container spacing={5} sx={{ mt: 3 }}>
-          <ListArticle header="Articles" articles={articles} />
-        </Grid>
+        {/* <ListArticle articles={articles} /> */}
       </DefaultLayout>
     </>
   );
